11060f9f3d6762e7f8b30de2572d1d1cb70116f6
Thomas Kelly <tkelly@scottlogic.com>
2016-11-23 20:46:28 +0400

    Demo of partial class stylecop warning

---
+++ PartialClassDemo/PartialClassDemo/PartialClass1.cs
@@ -0,0 +1,11 @@
+﻿namespace PartialClassDemo
+{
+    /// <summary>
+    ///     Partial class
+    /// </summary>
+    /// <typeparam name="T">Generic type</typeparam>
+    public partial class PartialClass<T>
+        where T : class
+    {
+    }
+}
